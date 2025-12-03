c79f3c26fb8d194a16ebc0133a6ef3bf229134c1
Barry Kadiatou <kadiatou.barry2.etu@a15p22.fil.univ-lille.fr>
2025-12-03 16:22:56 +0100

    ajout de test pour la creation d'objet velo

---
+++ test/java/vehicule/VeloFactoryTest.java
@@ -0,0 +1,38 @@
+package main.java.factory;
+
+import main.java.vehicule.Vehicule;
+import main.java.vehicule.Velo;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TestVeloFactory {
+
+    @Test
+    void testVeloNonNull() {
+        VeloFactory factory = new VeloFactory();
+        Vehicule v = factory.creerVehicule(1, "Velo", 200.0);
+        assertNotNull(v, "La factory ne doit pas retourner null");
+    }
+
+    @Test
+    void testVeloType() {
+        VeloFactory factory = new VeloFactory();
+        Vehicule v = factory.creerVehicule(1, "Velo", 200.0);
+        assertTrue(v instanceof Velo, "La factory doit créer un objet de type Velo");
+    }
+
+    @Test
+    void testVeloDescription() {
+        VeloFactory factory = new VeloFactory();
+        Vehicule v = factory.creerVehicule(1, "Velo", 200.0);
+        assertEquals("Velo 1", v.getDescription(), "La description doit être correctement initialisée");
+    }
+
+    @Test
+    void testVeloCout() {
+        VeloFactory factory = new VeloFactory();
+        Vehicule v = factory.creerVehicule(1, "Velo", 200.0);
+        assertEquals(200.0, v.getCout(), "Le coût doit être correctement initialisé");
+    }
+}
